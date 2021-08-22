aa8c9191283b222d8fc8b47342b21c4f7591bab0
loochek <m.ziganshin.zm@gmail.com>
2021-08-22 22:15:26 +0300

    Version 0.91

--- src/frontends/libretro/libretro_main.c
+++ src/frontends/libretro/libretro_main.c
@@ -78,7 +78,7 @@ void retro_get_system_info(struct retro_system_info *info)
 {
    memset(info, 0, sizeof(struct retro_system_info));
    info->library_name     = "gb";
-   info->library_version  = "0.9";
+   info->library_version  = "0.91";
    info->need_fullpath    = true;
    info->block_extract    = false;
    info->valid_extensions = "gb";
